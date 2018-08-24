6f05ab224b04e9153bad8d8f1cc35c0612e25320
乐升平 <lesp163@163.com>
2018-08-24 11:15:07 +0800

    Initial Commit

---
+++ SPPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPPickerView
+//
+//  Created by develop1 on 2018/8/24.
+//  Copyright © 2018年 Cookie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
